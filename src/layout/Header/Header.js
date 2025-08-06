35f2915d8699275acce91bc66965bc92b219f22d
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-06 10:42:50 +0900

    add: #1 Header Prototype생성

---
+++ src/layout/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div>
+            <div>
+                이건 헤더 테스트입니다.
+            </div>
+        </div>
+    );
+}
+
+export default Header;
