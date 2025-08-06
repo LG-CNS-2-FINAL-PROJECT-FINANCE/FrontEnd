c899ba0983f8ca8f8b789e8de21938e62ac7fc70
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-06 10:43:11 +0900

    add: #1 AdminHeader Prototype생성

---
+++ src/layout/Header/AdminHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AdminHeader(){
+    return(
+        <div>
+            <div>
+                어드민헤더 테스트입니다.
+            </div>
+        </div>
+    );
+}
+
+export default AdminHeader;
