30fee30e0d18057267a62ab99d97d207ab4ae095
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-06 10:41:49 +0900

    add: #1 #2 Footer, Header Router 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import ScrollToTop from './pages/common/ScrollToTop.js'
+import Layout from "./layout/Layout";
+import AdminLayout from "./layout/AdminLayout";
 
 function App() {
   return (
     <BrowserRouter>
+        <div className="flex flex-col min-h-screen px-[10%]">
         <ScrollToTop />
-        <Routes>
-            
-        </Routes>
+            <Routes>
+                <Route path='/' element={<Layout />}> {/*헤더, 푸터 고정 설정*/}
+
+
+
+                </Route>
+
+                {/*관리자 페이지 헤더*/}
+                <Route path='/admin' element={<AdminLayout />}>
+
+                </Route>
+            </Routes>
+        </div>
     </BrowserRouter>
   );
 }
