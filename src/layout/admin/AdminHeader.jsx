10a793f222ef2f9d7133398fab176ed7e26402d7
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-12 14:22:17 +0900

    add: #47 AdminHeader에 기능 추가/ 만약 로그인이 되어 있을 시 login/user로 이동되게 설정하고 안되어 있다면 admin/login으로 이동되게 설정

--- src/layout/admin/AdminHeader.jsx
+++ src/layout/admin/AdminHeader.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from "react-router-dom";
 
 function AdminHeader(){
+    const navigate = useNavigate();
+    const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
+
+    const TOKEN_KEY = 'JWT_TOKEN';
+
+    useEffect(() => {
+        const token = localStorage.getItem(TOKEN_KEY);
+        if (!token) {
+            setIsAdminLoggedIn(false);
+            return;
+        }
+
+        try {
+            setIsAdminLoggedIn(true);
+
+        } catch (err) {
+            console.error('토큰 검사 오류:', err);
+            setIsAdminLoggedIn(false);
+            localStorage.removeItem(TOKEN_KEY);
+        }
+    }, []);
+
+    const handleLogoClick = () => {
+        if (isAdminLoggedIn) {
+            navigate('/admin/user');
+        } else {
+            navigate('/admin/login');
+        }
+    };
+
+
     return(
-        <div>
-            <div>
-                어드민헤더 테스트입니다.
+        <div className="flex justify-between items-center w-full bg-white py-2 px-[3%] shadow-lg">
+            <div className="flex items-center space-x-6">
+                <div className="">
+                    <img
+                        src="/assets/adminlogo.png"
+                        alt="로고"
+                        className="w-28 h-15 min-h-1 min-w-1 hover:cursor-pointer"
+                        onClick={handleLogoClick}
+                    />
+                </div>
             </div>
         </div>
     );
