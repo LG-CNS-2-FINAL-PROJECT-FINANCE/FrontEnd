e128590d1b3faa0df0123e9ae636a52819b7a961
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-13 14:53:30 +0900

    Refactor: 버튼 컴포넌트 수정

--- src/component/button.jsx
+++ src/component/button.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function button(){
+function Button({children, size, onClick}){
     return(
         <div>
-            <button className="p-2 bg-red-500 rounded-xl text-amber-50 mt-1 h-14 w-36">
-                나는 버튼이예요
+            <button className={`p-2 bg-red-500 text-${size} rounded-xl text-amber-50 mt-1 h-14 w-36`}>
+                {children}
             </button>
         </div>
     )
 }
 
-export default button;
+export default Button;
