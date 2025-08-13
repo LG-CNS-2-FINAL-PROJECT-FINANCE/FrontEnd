1ab65044cd5814c9f3565491d2e2ff54a46fe5eb
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-13 14:53:03 +0900

    Refactor: #49 AdminHeader 수정

--- src/layout/admin/AdminHeader.jsx
+++ src/layout/admin/AdminHeader.jsx
@@ -1,42 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import {useNavigate} from "react-router-dom";
+import {AuthContext} from "../../context/AuthContext";
+import Button from "../../component/Button";
 
 function AdminHeader(){
     const navigate = useNavigate();
-    const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
-
-    const TOKEN_KEY = 'JWT_TOKEN';
-
-    useEffect(() => {
-        const token = localStorage.getItem(TOKEN_KEY);
-        if (!token) {
-            setIsAdminLoggedIn(false);
-            return;
-        }
-
-        try {
-            setIsAdminLoggedIn(true);
-
-        } catch (err) {
-            console.error('토큰 검사 오류:', err);
-            setIsAdminLoggedIn(false);
-            localStorage.removeItem(TOKEN_KEY);
-        }
-    }, []);
+    const { user } = useContext(AuthContext);
 
     const handleLogoClick = () => {
-        if (isAdminLoggedIn) {
-            navigate('/admin/user');
-        } else {
-            navigate('/admin/login');
-        }
+        if (user) navigate('/admin/user');
+        else navigate('/admin/login');
     };
-
 
     return(
         <div className="flex justify-between items-center w-full bg-white py-2 px-[3%] shadow-lg">
             <div className="flex items-center space-x-6">
                 <div className="">
+
                     <img
                         src="/assets/adminlogo.png"
                         alt="로고"
