778b57758927a56675cfafe7a1ed0ac2e698d3f0
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-13 14:52:39 +0900

    Add: #49 admin Layout 수정

--- src/layout/admin/AdminLayout.jsx
+++ src/layout/admin/AdminLayout.jsx
@@ -1,16 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import AdminHeader from './AdminHeader';
 import AdminFooter from './AdminFooter';
+import AdminSidebar from "../../pages/admin/adminComponent/AdminSidebar";
 
 function AdminLayout() {
     return (
         //style={{minHeight: '100vh'}}
-        <div>
-            <AdminHeader/>
+        <div className="min-h-screen flex flex-col">
+            <div className="w-full">
+                <AdminHeader/>
+            </div>
 
-            <main className="flex-grow">
-            <Outlet />
-            </main>
+            <div className="flex flex-1 py-4">
+                <aside className="w-64 border-r bg-white">
+                    <AdminSidebar />
+                </aside>
+
+                <main className="flex-1">
+                    <Outlet />
+                </main>
+            </div>
 
             <AdminFooter/>
         </div>
