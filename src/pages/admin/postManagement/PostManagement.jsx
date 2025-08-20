5f761a5f4b5752900082ebc91e56ce896c162f7f
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-08-20 16:30:05 +0900

    Chore: 구조변경

--- src/pages/admin/postManagement/PostManagement.jsx
+++ src/pages/admin/postManagement/PostManagement.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef, useContext } from "react";
-import { getPosts } from "../../../api/project_api";
+import { getPosts } from "../../../api/admin_project_api";
 import dayjs from "dayjs";
 import { AuthContext } from "../../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
