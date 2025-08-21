6a7a2affb7574a5fa1580e9600c7c547d5b3066a
Junho0225 <51467655+Junho0225@users.noreply.github.com>
2025-08-21 16:12:16 +0900

    feature: #89 프로젝트 수정 요청 추가

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import MyFavorites from "./pages/mypage/MyFavorites.jsx";
 import AccountManagement from "./pages/mypage/AccountManagement.jsx";
 import EditInfo from "./pages/mypage/EditInfo.jsx";
 import MyReports from "./pages/mypage/MyReports.jsx";
+import MyEditRequest from "./pages/mypage/MyEditRequest.jsx";
 import ReportManagement from "./pages/admin/ReportManagement";
 import PostManagement from "./pages/admin/postManagement/PostManagement";
 import Aml from "./pages/admin/Aml";
@@ -29,6 +30,7 @@ import SystemMonitoring from "./pages/admin/SystemMonitoring";
 import ProductRegistration from "./pages/product/ProductRegistration.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import MyProduct from "./pages/mypage/MyProduct.jsx";
+import KakaoConfirm from "./pages/login/KakaoConfirm.jsx";
 
 
 
@@ -46,6 +48,7 @@ function App() {
               <Route index element={<Home />}></Route>
               {/*로그인 페이지*/}
               <Route path="/login/:id" element={<Login />}></Route>
+              <Route path="/login/kakaoConfirm" element={<KakaoConfirm />}></Route>
               {/*역할 선택 페이지*/}
               <Route path="/select-role" element={<RoleSelectionPage />} />
 
@@ -59,7 +62,7 @@ function App() {
               {/*Market*/}
               <Route path="/market" element={<Market />} />
               <Route path="/market/:id" element={<MarketDetail />} />
-              
+
               {/*MyPage*/}
               <Route path="/my-profile" element={<MyPage />} />
               <Route path="/my-investments" element={<MyInvestments />} />
@@ -67,24 +70,25 @@ function App() {
               <Route path="/account-management" element={<AccountManagement />} />
               <Route path="/edit-info" element={<EditInfo />} />
               <Route path="/my-reports" element={<MyReports />} />
+              <Route path="/my-edit-request" element={<MyEditRequest />} />
               <Route path="/my-product" element={<MyProduct />} />
 
               {/*Asset*/}
               <Route path="/asset" element={<Asset />} />
-                
+
             </Route>
 
-          {/*관리자 로그인 페이지*/}{/*얘는 헤더가 없음*/}
-          <Route path="/admin/login" element={<AdminLogin />} />
-          {/*관리자 페이지 헤더*/}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route path="user" element={<UserManagement />}></Route>
-            <Route path="reports" element={<ReportManagement />}></Route>
-            <Route path="posts" element={<PostManagement />}></Route>
-            <Route path="aml" element={<Aml />}></Route>
-            <Route path="frauddetection" element={<FraudDetection />}></Route>
-            <Route path="settings" element={<Setting />}></Route>
-            <Route path="systemmonitoring" element={<SystemMonitoring />}></Route>            
+            {/*관리자 로그인 페이지*/}{/*얘는 헤더가 없음*/}
+            <Route path="/admin/login" element={<AdminLogin />} />
+            {/*관리자 페이지 헤더*/}
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route path="user" element={<UserManagement />}></Route>
+              <Route path="reports" element={<ReportManagement />}></Route>
+              <Route path="posts" element={<PostManagement />}></Route>
+              <Route path="aml" element={<Aml />}></Route>
+              <Route path="frauddetection" element={<FraudDetection />}></Route>
+              <Route path="settings" element={<Setting />}></Route>
+              <Route path="systemmonitoring" element={<SystemMonitoring />}></Route>
 
             </Route>
           </Routes>
