75848e072483721b9ab6fcae7d4d606187c3c491
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-09-01 17:55:57 +0900

    Add: #126 게시물 관리 API 연결

--- src/pages/admin/postManagement/PostAllManagement.jsx
+++ src/pages/admin/postManagement/PostAllManagement.jsx
@@ -150,13 +150,15 @@ export default function PostAllManagement() {
                                             <td className="px-4 py-3 text-sm border-b">{formatDate(p.endDate) ?? '-'}</td>
                                             <td className="px-4 py-3 text-sm border-b">
                                                     <span className={`inline-flex items-center px-2 py-1 rounded-full text-xs font-medium ${(p.status) === 'APPROVED' ? 'bg-green-100 text-green-700' :
-                                                        (p.status) === 'REJECTED' ? 'bg-red-100 text-red-700' :
+                                                        (p.state) === 'OPEN' ? 'bg-red-100 text-red-700' :
                                                             'bg-yellow-100 text-yellow-700'
+                                                        
+                                                        //OPEN / FUNDING_LOCKED / TRADING / DISTRIBUTION_READY / DISTRIBUTING / CLOSED / TEMPORARY_STOP
                                                     }`}>
-                                                        {p.status ?? '-'}
+                                                        {p.state ?? '-'}
                                                     </span>
                                             </td>
-                                            <td className="px-4 py-3 text-sm border-b">{p.type ?? '-'}</td>
+                                            <td className="px-4 py-3 text-sm border-b">{p.visiblity ?? '-'}</td>
                                         </tr>
                                     );
                                 })
