fa3c3538ba69a50d43f5561f8eefd8298980ebc7
HyoJun Ahn <59754350+hyojunahn111@users.noreply.github.com>
2025-09-03 15:26:57 +0900

    Refactor: 헤더 고정

--- src/layout/user/Layout.jsx
+++ src/layout/user/Layout.jsx
@@ -7,10 +7,12 @@ function Layout() {
         // 레이아웃 최대높이 (사이트 최소높이 브라우저 창 크기 - 필요시 변경 가능)
         //style={{minHeight: '100vh'}}
         <div className="flex flex-col min-h-screen">
-            <Header />
+            <header className="fixed top-0 left-0 w-full z-50">
+                <Header />
+            </header>
 
-            <main className="flex-grow px-[10%] ">
-            <Outlet />
+            <main className="flex-grow px-[10%] pt-16">
+                <Outlet />
             </main>
             
             <Footer />
